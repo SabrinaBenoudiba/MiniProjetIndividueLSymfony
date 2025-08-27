7e60c78175260063331c78b0b769e43fca9d7589
SabrinaBenoudiba <sabrina.benoudiba@gmail.com>
2025-08-27 16:45:23 +0200

    update movieController and templates

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -13,6 +13,7 @@ use Symfony\Component\HttpFoundation\Request;
 final class MovieController extends AbstractController
 {
     #[Route('/movie', name: 'app_movie')]
+   
     public function index(): Response
     {
         return $this->render('movie/index.html.twig', [
@@ -20,30 +21,42 @@ final class MovieController extends AbstractController
         ]);
     }
 
-    #region New
+    #region New & Edit
+    #[Route('/movie/edit/{id}', name: 'app_movie_edit')]
     #[Route('/movie/new', name: 'app_movie_new')]
-    public function new(Request $request, EntityManagerInterface $em): Response
+    public function new(?Movie $movie, Request $request, EntityManagerInterface $em): Response
     {
-        $movie = new Movie();
+        // On prépare le message flash qui nous servira plus tard : soit une modification, soit un ajout en fonction de la valeur de notre object projet. En effet s'il est initialisé c'est qu'on a passé l'id d'un projet existant : c'est une modification. sinon l'objet est null : c'est un ajout
+        $status = isset($movie) ? "modifié" : "ajouté";
+
+        // Même logique ici pour le titre de la page : 
+        $title = isset($movie) ? "Modifier projet : ". $movie->getMovieTitle() : "Ajouter un projet";
+        $movie = $movie ?? new Movie();
+
         $form = $this->createForm(MovieType::class, $movie);
+
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            
             $em->persist($movie);
             $em->flush();
 
             // Redirige après création
-            return $this->redirectToRoute('app_movie');
+            // return $this->redirectToRoute('app_movie');
+            
+            $this->addFlash('success', 'Movie'. $status . ' !');  
+            return $this->redirectToRoute('app_movie_show', ['id' => $movie->getId()]);
         }
-
+        
         return $this->render('movie/new.html.twig', [
             'form' => $form->createView(),
         ]);
     }
-    #endregion New
+    #endregion New & Edit
 
     #region Show
-    #[Route('/{id}/movie', name: 'app_movie_show', methods: ['GET'])]
+    #[Route('/movie/{id}', name: 'app_movie_show', methods: ['GET'])]
     public function show(Movie $movie): Response
     {
     return $this->render('movie/show.html.twig', [
@@ -52,5 +65,19 @@ final class MovieController extends AbstractController
     }
     #endregion Show
 
+    #region DELETE
+    #[Route('/movie/{id}', name: 'app_movie_delete')]
+    public function delete(Request $request, Movie $movie, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$movie->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($movie);
+            $entityManager->flush();
+
+            $this->addFlash('danger', "Le produit a bien été supprimé");
+        }
+
+        return $this->redirectToRoute('app_movie', [], Response::HTTP_SEE_OTHER);
+    }
+    #endregion DELETE
 
 }
